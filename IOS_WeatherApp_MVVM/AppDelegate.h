0f2708dc8a27042e859b8deab9d2a8cee6bed810
Tony <inbox1.tony@gmail.com>
2016-04-18 15:45:49 +1000

    Initial Commit

---
+++ IOS_WeatherApp_MVVM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOS_WeatherApp_MVVM
+//
+//  Created by Tony Thomas on 18/04/16.
+//  Copyright © 2016 Tony Thomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOS_WeatherApp_MVVM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IOS_WeatherApp_MVVM
+//
+//  Created by Tony Thomas on 18/04/16.
+//  Copyright © 2016 Tony Thomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
